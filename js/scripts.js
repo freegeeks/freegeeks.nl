f17f44ab4b65776c2cd0448868751759f4ab26cb
renasboy <renasboy@gmail.com>
2013-07-25 07:44:48 +0200

    Some cleanup in the JS, using rem instead of em

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,7 @@
 
 var fixed = false;
 $(document).scroll(function() {
+    // TODO 90 has to be calculated per screensize
     if( $(this).scrollTop() >= 90 ) {
         if( !fixed ) {
             fixed = true;
