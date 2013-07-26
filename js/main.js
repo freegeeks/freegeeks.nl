cddb8046acbedee6fb050026a92306e6590734c9
renasboy <renasboy@gmail.com>
2013-07-26 18:59:41 +0200

    Added very first skrollr

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-var fixed = false;
-$(document).scroll(function() {
-    // TODO 90 has to be calculated per screensize
-    if( $(this).scrollTop() >= 90 ) {
-        if( !fixed ) {
-            fixed = true;
-			
-            $('header')
-                .addClass("collapsed")
-                .addClass("fade");
-		}                                          
-    } else {
-        if( fixed ) {
-            fixed = false;
-			$('header').removeClass("collapsed");
-			
-        }
-    }
+skrollr.init({
+    forceHeight: false
 });
